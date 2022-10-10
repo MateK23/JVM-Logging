377782a025f7e7c2a7ec4dfc848143ceb480ef97
MateK23 <mate.warface@gmail.com>
2022-10-10 20:23:31 +0400

    init

---
+++ src/com/matek/logging/Main.java
@@ -0,0 +1,16 @@
+package com.matek.logging;
+
+//import java.util.logging.Logger;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+
+public class Main {
+
+    final static Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+    }
+
+}
