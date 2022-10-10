c2c1245afe3be12c31939f3cd9687d6e0d729163
MateK23 <mate.warface@gmail.com>
2022-10-10 21:06:46 +0400

    HW

--- src/com/matek/logging/Main.java
+++ src/com/matek/logging/Main.java
@@ -3,13 +3,23 @@ package com.matek.logging;
 //import java.util.logging.Logger;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.Priority;
-
 public class Main {
 
     final static Logger logger = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
+        logger.trace("Trace");
+        logger.debug("Debug");
+        logger.info("Info");
+        logger.error("Error");
+        logger.fatal("Fatal");
+        logger.warn("Warn");
+
+
+        // Davalebebi:
+        // exceptionebit da logebit ramis sheqmna
+        //
+        // reflectionebit da anotaciebit rame aplikaciis sheqmnda
 
     }
 
